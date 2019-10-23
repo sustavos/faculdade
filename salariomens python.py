7cf21e8ac9cbb00ccbb6f11110bf5fd68d883886
nicolasrls <nicolas.261195@gmail.com>
2019-10-23 00:23:57 -0300

    arquivos faculdade

---
+++ salariomens python.py
@@ -0,0 +1,12 @@
+valorhora = 4
+diastrab = 30
+horastrab = 8
+
+salariomens = valorhora*diastrab*horastrab
+
+
+nome = "nicolas"
+
+
+print ("Funcionario : ", nome)
+print("Salario: ", salariomens) 
