489f2aa26fece0e72b5a3ffe9ff6b7c3cc5bd27b
nicolasrls <nicolaufacul@gmail.com>
2019-11-27 23:06:07 -0300

    Aula dia 26/11 FPGA E CL

--- Circuitos Lógicos/FPGA/meudisplay/meudisplay.vhd
+++ Circuitos Lógicos/FPGA/meudisplay/meudisplay.vhd
@@ -11,12 +11,11 @@ end meudisplay;
 --------------------------------------
 architecture dataflow of meudisplay is
 begin 
-	Sa<= A or C or (B xnor D);
-	Sb<= (not B) or (C xnor D);
-	Sc<= B or(not C) or D;
-	Sd<= A or((not B) and (not D))or((not B) and C) or (C and (not D)) or (B and (not C) and D);
-	Se<= ((not B) and (not D)) or (C and (not D));
-	Sf<= A or ((not C) and (not D)) or (B and (not C)) or (B and (not D));
-	Sg<= A or (B xor C) or (C and (not D));
-	
+	Sa<= not((A or C) or (B xnor D));
+	Sb<= not((not B) or (C xnor D));
+	Sc<= not(B or(not C) or D);
+	Sd<= not(A or((not B) and (not D))or((not B) and C) or (C and (not D)) or (B and (not C) and D));
+	Se<= not(((not B) and (not D)) or (C and (not D)));
+	Sf<= not(A or ((not C) and (not D)) or (B and (not C)) or (B and (not D)));
+	Sg<= not(A or (B xor C) or (C and (not D)));
 	end dataflow;
